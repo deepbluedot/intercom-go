0be988822efa0e6b562cf61dbf9d1c23c7145ada
김진경 <damon@damon.local>
2023-03-01 20:39:01 -0800

    fixed to page params

--- page.go
+++ page.go
@@ -2,7 +2,9 @@ package intercom
 
 // PageParams determine paging information to and from the API
 type PageParams struct {
-	Page       int64 `json:"page" url:"page,omitempty"`
-	PerPage    int64 `json:"per_page" url:"per_page,omitempty"`
-	TotalPages int64 `json:"total_pages" url:"-"`
+	Page          int64       `json:"page" url:"page,omitempty"`
+	StartingAfter string      `json:"starting_after,omitempty"`
+	PerPage       int64       `json:"per_page" url:"per_page,omitempty"`
+	TotalPages    int64       `json:"total_pages" url:"-"`
+	Next          *PageParams `json:"next,omitempty"`
 }
